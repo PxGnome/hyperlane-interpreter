74b767629cdb8e87538af7b072d2a2be0940e39b
pxgnome <pxgnome@gmail.com>
2023-05-27 01:42:27 +0900

    Updated hardhat task

--- tasks/deploy_lz.ts
+++ tasks/deploy_lz.ts
@@ -4,7 +4,6 @@ import { Signer } from "@ethersproject/abstract-signer";
 import hlAddressesJson from '../environment/hyperlane/addresses.json';
 import lzDomainIdJson from '../environment/layerzero/domainId.json';
 
-
 task("lzrouter_deploy", "deploy router")
   .addPositionalParam("isMainnet")
   .setAction(async (_taskArgs, hre) => {
@@ -12,8 +11,6 @@ task("lzrouter_deploy", "deploy router")
 
     const owner = accounts[0];
     const networkName = hre.network.name;
-
-
 
     console.log("Deploying on network: %s", networkName);
 
@@ -141,4 +138,55 @@ task("lzrouter_enroll", "enroll router")
     var addressAsBytes32 = hre.ethers.utils.defaultAbiCoder.encode(["address"], [enrollRouterAddr])
     await lzRouter.connect(owner).enrollRemoteRouter(hlDestinationDomain, addressAsBytes32);
     console.log("Enroll complete");
+  });
+
+task("lzrouter_sendMessage", "Send Message using Lz Router")
+  .addPositionalParam("originRouterAddr")
+  .addPositionalParam("destinationNetwork")
+  .addPositionalParam("destinationAddr")
+  .addPositionalParam("message")
+  .setAction(async (_taskArgs, hre) => {
+    const accounts: Signer[] = await hre.ethers.getSigners();
+
+    const owner = accounts[0];
+
+    // npx hardhat lzrouter_sendMessage 0xDddD17bDeF830103846f89cF61d362A689195c29 mumbai 0x695b3e0da6823093d4A7d6628CdDFb37aa8CA907 abc --network sepolia
+
+    const networkName = hre.network.name;
+
+    console.log("Executing on network: %s", networkName);
+
+    const originRouterAddr = _taskArgs.originRouterAddr;
+    const destinationNetwork = _taskArgs.destinationNetwork;
+    const destinationAddr = _taskArgs.destinationAddr;
+    const message = _taskArgs.message;
+
+    console.log("Send Message to: %s", destinationAddr);
+    console.log("Message: %s", message);
+
+    const lzDestinationDomain = lzDomainIdJson[destinationNetwork]['layerzero'];
+
+    const lzRouter = await hre.ethers.getContractAt("LayerZeroRouter", originRouterAddr);
+
+    var payload = hre.ethers.utils.arrayify(hre.ethers.utils.toUtf8Bytes(message));
+
+    var adapterParams = hre.ethers.utils.arrayify(hre.ethers.utils.toUtf8Bytes(''));
+
+    var gasPayment = await lzRouter.estimateFees(lzDestinationDomain, destinationAddr, payload, false, adapterParams);
+
+    console.log("Gas Value: %s", gasPayment.nativeFee);
+
+    var destinationAddrAsBytes32 = hre.ethers.utils.solidityPack(["address", "address"], [originRouterAddr, destinationAddr]);
+
+    var messageId = await lzRouter.send(
+      lzDestinationDomain,
+      destinationAddrAsBytes32,
+      payload,
+      owner.address,
+      hre.ethers.constants.AddressZero,
+      adapterParams,
+      { value: gasPayment.nativeFee }
+    );
+
+    console.log("Message Sent %s", messageId);
   });
